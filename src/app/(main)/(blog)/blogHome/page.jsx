53dcef8fbe759092e0b9d69cdd83f15ef421b747
heechae <chaehee15@naver.com>
2025-06-24 17:48:59 +0900

    feat: 블로그 홈 화면 추가

--- src/app/(main)/(blog)/blogHome/page.jsx
+++ src/app/(main)/(blog)/blogHome/page.jsx
@@ -1,8 +1,19 @@
-const BlogList = () => {
-    return(
-        <div>
-            <h1>블로그 목록</h1>
-        </div>
+"use client";
+import { useState } from "react";
+import ChoiceMenu from "./ChoiceMenu";
+import BlogList from "./BlogList";
+import blogs from "./blogs.json";
+
+const categories = [...new Set(blogs.map(b => b.category))];
+
+export default function BlogHome() {
+    const [selected, setSelected] = useState("전체");
+    const filtered = selected === "전체" ? blogs : blogs.filter(b => b.category === selected);
+
+    return (
+        <>
+            <ChoiceMenu categories={["전체", ...categories]} selected={selected} onSelect={setSelected} />
+            <BlogList blogs={filtered} />
+        </>
     );
-}
-export default BlogList;
+};
--- src/app/(main)/page.jsx
+++ src/app/(main)/page.jsx
@@ -4,7 +4,10 @@ const MainPage = () => {
     return(
         <div>
             <h1>네이버 블로그</h1>
-            <button>블로그 목록</button> <br />
+            <Link href="/blogHome">
+            <button>블로그 목록</button>
+            </Link>
+            <br />
             <button>주제별 보기</button>
         </div>
     );
