753519ab21aa96e4088f346146008ab14d01b0fa
FerryLa <rlawl4240@gmail.com>
2025-06-25 16:00:10 +0900

    commit

--- src/app/(main)/page.jsx
+++ src/app/(main)/page.jsx
@@ -6,7 +6,7 @@ const MainPage = () => {
             <h1>네이버 블로그</h1>
             <button>블로그 목록</button> <br />
             <button>주제별 보기</button>
-            <Link href="/(article)">
+            <Link href="/edit">
                 <button>글 쓰기 메인으로 이동</button>
             </Link>
 
