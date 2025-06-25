c711ea02af17afa14e9a262544b1d4d860941e3d
heechae <chaehee15@naver.com>
2025-06-25 17:30:56 +0900

    feat: 블로그 홈 화면 완료

--- src/app/(main)/(blog)/blogHome/BlogList.jsx
+++ src/app/(main)/(blog)/blogHome/BlogList.jsx
@@ -1,47 +1,51 @@
 import Image from "next/image";
+import Link from "next/link";
+import './styles.css'
 
 export default function BlogList({blogs}) {
     return(
-        // <div style={{margin: "16px"}}>
-        //         {blogs.map(blog => (
-        //     <div key={blog.id}>
-        //             <p>{blog.author}  <br /> {blog.date} <br /> {blog.title} <br /> {blog.content}</p>
-        //         <hr />
-        //     </div>
-        //         ))}
-        // </div>
         <div style={{ margin: "16px" }}>
             {blogs.map(blog => (
                 <div
                     key={blog.id}
                     style={{
-                        marginBottom: "32px",
+                        marginBottom: "30px",
                         borderBottom: "1px solid #eee",
-                        paddingBottom: "24px"
+                        paddingBottom: "25px"
                     }}
                 >
-                    <div style={{alignItems: "center", marginBottom: "16px"}}>
-                    {/* 프로필 이미지 */}
-                    <Image src={blog.authorImage} alt={"프로필 이미지"} width={40} height={40} style={{ borderRadius: "50%", marginRight: "16px" }} />
-                    {/* 텍스트 영역 */}
-                        <div style={{ fontWeight: "bold", fontSize: "15px", marginBottom: "2px" }}>
-                            {blog.author}
-                        </div>
-                        <div style={{ color: "#888", fontSize: "12px", marginBottom: "12px" }}>
-                            {blog.date}
+                    {/* 작성자/날짜/이미지 영역 */}
+                    <div style={{ display: "flex", alignItems: "flex-start", marginBottom: "12px" }}>
+                        {/* 프로필 이미지에 링크 */}
+                        <Link href="/blogDetail" className="underline-link">
+                            <Image
+                                src={blog.authorImage}
+                                alt="프로필 이미지"
+                                width={35}
+                                height={35}
+                                style={{ borderRadius: "50%", marginRight: "10px", verticalAlign: "middle" }}
+                            />
+                        </Link>
+                        <div>
+                            <Link href="/blogDetail" className="underline-link" style={{ fontWeight: "bold", fontSize: "14px" }}>
+                                {blog.author}
+                            </Link>
+                            <div style={{ color: "#888", fontSize: "10px" }}>{blog.date}</div>
                         </div>
                     </div>
-                    <div>
-                        <div style={{ fontWeight: "bold", fontSize: "18px", marginBottom: "12px" }}>
+                    {/* 제목/본문 영역 */}
+                    <div style={{ marginLeft: "0" }}>
+                        {/* 제목에 링크 */}
+                        <Link href="/blogDetail" className="underline-link" style={{ fontWeight: "bold", fontSize: "17px", marginBottom: "10px", display: "block" }}>
                             {blog.title}
-                        </div>
-                        <div style={{ color: "#444", fontSize: "15px", lineHeight: "1.7" }}>
+                        </Link>
+                        {/* 본문에 링크 */}
+                        <Link href="/blogDetail" className="underline-link" style={{ color: "#444", fontSize: "13px", lineHeight: "1.7", display: "block" }}>
                             {blog.content}
-                        </div>
+                        </Link>
                     </div>
                 </div>
             ))}
         </div>
-
     );
 }
