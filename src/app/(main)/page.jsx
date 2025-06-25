d8cf99086bb39fd016a61701743734d71b66a9ae
FerryLa <rlawl4240@gmail.com>
2025-06-25 17:43:13 +0900

    fix 'blogEditor'
    - 기능 폴더 틀 완성
    - 블로그 편집(글쓰기) 페이지 초안
    - 이미지 파일 업로드..

--- src/app/(main)/page.jsx
+++ src/app/(main)/page.jsx
@@ -6,7 +6,7 @@ const MainPage = () => {
             <h1>네이버 블로그</h1>
             <Link href="/blogHome"><button>블로그 홈</button></Link>
             <button>주제별 보기</button>
-            <Link href="/edit">
+            <Link href="/blogEditor">
                 <button>글 쓰기 메인으로 이동</button>
             </Link>
 
